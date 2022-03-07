f32dd6d614db3cb3f42d1397e54f64304c4e2aae
mdomar2998 <95675143+Mdomar2998@users.noreply.github.com>
2022-03-07 07:12:54 +0000

    p3

--- p3final.c
+++ p3final.c
@@ -4,7 +4,7 @@ int input_number();
 int is_prime(int n);
 void output(int n, int is_prime);
 */
-
+/*
 #include <stdio.h>
 #include <math.h>
 int input()
@@ -45,4 +45,47 @@ int main()
   output(n,isp);
   return 0;
   }
+}*/
+
+
+#include<stdio.h>
+
+int input_n_and_r(int *n,int *r)
+{
+ printf("Enter the value of n:");
+ scanf("%d",n);
+ printf("Enter the values of r:");
+ scanf("%d",r);
+}
+
+int ncr(int n,int r)
+{
+  int nfact=1,rfact=1,nrfact=1,ncr;
+  for(int i=1;i<=n;i++)
+    {
+      nfact*=i;
+    }
+  for(int j=1;j<=r;j++)
+    {
+      rfact*=j;
+    }
+  for(int k=1;k<=(n-r);k++)
+    {
+      nrfact*=k;
+    }
+  ncr=nfact/(rfact*nrfact);
+  return ncr;
+}
+
+void output(int n,int r,int result)
+{
+  printf("The nCr value of %dC%d is: %d",n,r,result);
+}
+
+int main()
+{
+  int n,r,result;
+  input_n_and_r(&n,&r);
+  result=ncr(n,r);
+  output(n,r,result);
 }
